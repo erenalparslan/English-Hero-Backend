fbe5d1d60850ee1166779af663313f8d91ee4ba6
Eren <erenalparslan52@gmail.com>
2024-07-19 19:13:56 +0300

    security and login page

---
+++ src/main/java/com/example/englishhero/English/Hero/businnes/concretes/AuthenticationService.java
@@ -0,0 +1,140 @@
+package com.example.englishhero.English.Hero.businnes.concretes;
+
+import com.example.englishhero.English.Hero.config.JwtService;
+import com.example.englishhero.English.Hero.core.dtos.AuthenticationRequest;
+import com.example.englishhero.English.Hero.core.dtos.AuthenticationResponse;
+import com.example.englishhero.English.Hero.core.dtos.RegisterRequest;
+import com.example.englishhero.English.Hero.core.utilities.security.token.Token;
+import com.example.englishhero.English.Hero.core.utilities.security.token.TokenRepository;
+import com.example.englishhero.English.Hero.core.utilities.security.token.TokenType;
+import com.example.englishhero.English.Hero.entities.Role;
+import com.example.englishhero.English.Hero.entities.User;
+import com.example.englishhero.English.Hero.entities.dtos.CheckTokenDTO;
+import com.example.englishhero.English.Hero.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+  private final UserRepository repository;
+  private final TokenRepository tokenRepository;
+  private final PasswordEncoder passwordEncoder;
+  private final JwtService jwtService;
+  private final AuthenticationManager authenticationManager;
+
+  public AuthenticationResponse register(RegisterRequest request) {
+    var user = User.builder()
+        .name(request.getName())
+        .email(request.getEmail())
+        .password(passwordEncoder.encode(request.getPassword()))
+        .role(request.getRole() != null ? request.getRole(): Role.USER)
+        .build();
+    var savedUser = repository.save(user);
+    var jwtToken = jwtService.generateToken(user);
+    var refreshToken = jwtService.generateRefreshToken(user);
+    saveUserToken(savedUser, jwtToken);
+    return AuthenticationResponse.builder()
+        .accessToken(jwtToken)
+            .refreshToken(refreshToken)
+        .build();
+  }
+
+  public AuthenticationResponse authenticate(AuthenticationRequest request) {
+    authenticationManager.authenticate(
+        new UsernamePasswordAuthenticationToken(
+            request.getEmail(),
+            request.getPassword()
+        )
+    );
+    var user = repository.findByEmail(request.getEmail())
+        .orElseThrow();
+    var jwtToken = jwtService.generateToken(user);  
+    var refreshToken = jwtService.generateRefreshToken(user);
+    revokeAllUserTokens(user);
+    saveUserToken(user, jwtToken);
+    return AuthenticationResponse.builder()
+        .accessToken(jwtToken)
+            .refreshToken(refreshToken)
+            .role(user.getRole())
+        .build();
+  }
+
+  private void saveUserToken(User user, String jwtToken) {
+    var token = Token.builder()
+        .user(user)
+        .token(jwtToken)
+        .tokenType(TokenType.BEARER)
+        .expired(false)
+        .revoked(false)
+        .build();
+    tokenRepository.save(token);
+  }
+
+  private void revokeAllUserTokens(User user) {
+    var validUserTokens = tokenRepository.findAllValidTokenByUser(user.getId());
+    if (validUserTokens.isEmpty())
+      return;
+    validUserTokens.forEach(token -> {
+      token.setExpired(true);
+      token.setRevoked(true);
+    });
+    tokenRepository.saveAll(validUserTokens);
+  }
+
+  public void refreshToken(
+          HttpServletRequest request,
+          HttpServletResponse response
+  ) throws IOException {
+    final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+    final String refreshToken;
+    final String userEmail;
+    if (authHeader == null ||!authHeader.startsWith("Bearer ")) {
+      return;
+    }
+    refreshToken = authHeader.substring(7);
+    userEmail = jwtService.extractUsername(refreshToken);
+    if (userEmail != null) {
+      var user = this.repository.findByEmail(userEmail)
+              .orElseThrow();
+      if (jwtService.isTokenValid(refreshToken, user)) {
+        var accessToken = jwtService.generateToken(user);
+        revokeAllUserTokens(user);
+        saveUserToken(user, accessToken);
+        var authResponse = AuthenticationResponse.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+        new ObjectMapper().writeValue(response.getOutputStream(), authResponse);
+      }
+    }
+  }
+
+  public boolean isAuthenticated(CheckTokenDTO tokenDTO) {
+
+    String originalToken = tokenDTO.getToken();
+    String userEmail = jwtService.extractUsername(originalToken);
+    if (userEmail != null) {
+     var user = this.repository.findByEmail(userEmail)
+              .orElseThrow();
+      if (jwtService.isTokenValid(originalToken, user)) {
+        return true;
+      }
+      return false;
+    }
+    return false;
+    
+  }
+  
+}
